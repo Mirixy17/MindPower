209a3a7245fe1f844a17d038019e2ec32c03064e
Mirixy17 <64924145+Mirixy17@users.noreply.github.com>
2020-05-06 20:54:38 +0200

    Create main.cpp
    prvi commit

---
+++ 1 mnm fun/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void main()
+{
+	int mnm = 6;
+
+	mnm = mnm - 1;
+	mnm = mnm - 1;
+	mnm = mnm - 1;
+	mnm = mnm - 1;
+	mnm = mnm - 1;
+	mnm = mnm - 1;
+
+	std::cout << mnm;
+}
