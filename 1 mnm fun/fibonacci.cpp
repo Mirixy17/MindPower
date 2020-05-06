bc465df3c5ad0898f3fcb303cad3cbe3ad2ce0f7
Mirixy17 <64924145+Mirixy17@users.noreply.github.com>
2020-05-06 22:05:32 +0200

    Create fibonacci.cpp

---
+++ 1 mnm fun/fibonacci.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+//Funkcija - stoVraca ImeFunkcije stoPrima
+int FibBroj(int prvi, int drugi)
+{
+	int zbroj = prvi + drugi;
+	std::cout << zbroj << std::endl;
+	return zbroj;
+}
+
+void main()
+{
+	int prvi_broj = 1;
+	int drugi_broj = 1;
+	
+	std::cout << prvi_broj << std::endl;
+	std::cout << drugi_broj << std::endl;
+	
+	int treci_broj = 0;
+
+	while (treci_broj < 10000)
+	{
+		treci_broj = FibBroj(prvi_broj, drugi_broj);
+		drugi_broj = prvi_broj;
+		prvi_broj = treci_broj;
+	}
+}
