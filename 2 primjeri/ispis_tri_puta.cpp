94bb802146089a89327a4f348d86315307d00a5b
Luka Rolak <lukarolak2@gmail.com>
2020-05-16 22:02:19 +0200

    Dodao primjere koje smo radili 16.05

---
+++ 2 primjeri/ispis_tri_puta.cpp
@@ -0,0 +1,19 @@
+#include <pch.h>
+#include <iostream>
+ 
+int main()
+{
+	int broj = 1;
+
+	broj++;
+	std::cout << broj;
+
+	broj++;
+	std::cout << broj;
+
+	broj++;
+	std::cout << broj;
+
+
+	//uvecaj varijablu za jedan i ispisi njezinu vijednost, ponovi 3 puta
+}
---
+++ 2 primjeri/uvecavanje_s_uvijetom.cpp
@@ -0,0 +1,13 @@
+﻿#include <pch.h>
+#include <iostream>
+ 
+int main()
+{
+	int broj = 0;
+	while (broj < 10)
+	{
+		std::cout << broj << "\n";
+		broj++;
+	}
+}
+//Uvecaj i ispisi broj dok je manji od deset
