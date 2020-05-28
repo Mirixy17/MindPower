024295afa1b84ae70e622a4c0dc4dc1abae3f16d
Mirixy17 <64924145+Mirixy17@users.noreply.github.com>
2020-05-28 10:29:30 +0200

    Update ispisivanje_rijeci_for_loop.cpp

--- 3 primjeri/ispisivanje_rijeci_for_loop.cpp
+++ 3 primjeri/ispisivanje_rijeci_for_loop.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
 
+int main()
+{
+	int broj = 0;
+
+	std::string rijec;
+	std::cin >> rijec;
+
+	for (rijec[broj]; broj < rijec.size(); broj++) 
+	{
+		std::cout << rijec[broj] << "\n";
+
+	}
+}
