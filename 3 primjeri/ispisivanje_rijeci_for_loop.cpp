0d250843a549fe1402b5072a8b599f8a710f71ae
Luka Rolak <lukarolak2@gmail.com>
2020-05-30 09:39:41 +0200

    Update ispisivanje_rijeci_for_loop.cpp

--- 3 primjeri/ispisivanje_rijeci_for_loop.cpp
+++ 3 primjeri/ispisivanje_rijeci_for_loop.cpp
@@ -13,3 +13,16 @@ int main()
 
 	}
 }
+
+#include <iostream>
+
+int main()
+{
+	std::string rijec;
+	std::cin >> rijec;
+	
+	for (int broj = 0; broj < rijec.size(); broj++)
+	{
+		std::cout << rijec[broj] << "\n";
+	}
+}
