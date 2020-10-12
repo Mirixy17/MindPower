ad2ad4598fa6fe31e8f26531ace21c62e42c6606
Mirixy17 <64924145+Mirixy17@users.noreply.github.com>
2020-10-12 10:15:14 +0200

    Update Opcija1 sa main.cpp

--- funkcije/Opcija1 sa main.cpp
+++ funkcije/Opcija1 sa main.cpp
@@ -1,3 +1,5 @@
+MAIN JE UVIJEK U CPP FILE-U; NE U HEADERU; 
+
 
 
 U main.cpp
@@ -45,3 +47,33 @@ u Fizz.h
 #pragma once
 
 void FizzBuzz(int broj);
+
+AKO IMAMO VOID MAIN NA KRAJU KODA ONDA MOZE I BEZ HEDERA; NPR
+
+#include <iostream>
+#include "Fizz.h"
+
+void FizzBuzz(int broj)
+{
+	if (broj % 5 == 0 && broj % 3 == 0)
+	{
+		std::cout << "fizzbuzz" << std::endl;
+	}
+	else if (broj % 5 == 0)
+	{
+		std::cout << "buzz" << std::endl;
+	}
+	else if (broj % 3 == 0)
+	{
+		std::cout << "fizz" << std::endl;
+	}
+	else
+	{
+		std::cout << broj << std::endl;
+	}
+}
+
+void main()
+{
+	FizzBuzz(30);
+}
