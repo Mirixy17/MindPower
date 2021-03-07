b6a810cec0c3cf9a750d932b93696b316bfb8f2f
Luka Rolak <lukarolak2@gmail.com>
2021-03-07 11:33:41 +0100

    Zadatak1

---
+++ Zadatci/Zadatak1/NeRijeseniZadatak.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif // _WIN32
+
+void Spavaj(int brojMilisekundi)
+{
+#ifdef _WIN32
+	Sleep(brojMilisekundi);
+#else
+	usleep(brojMilisekundi * 1000);
+#endif // _WIN32
+}
+
+void Ispuni(std::vector<char>& polje)
+{
+	polje.resize(5);
+	polje[0] = '*';
+	for (int i = 1; i < 5; i++)
+	{
+		polje[i] = '_';
+	}
+}
+
+void Ispisi(std::vector<char>& polje)
+{
+	for (int i = 0; i < polje.size(); i++)
+	{
+		std::cout << polje[i];
+	}
+	std::cout << '\n';
+}
+
+void PomakniZvjezdicu(std::vector<char>& polje)
+{
+	while (polje[polje.size() - 1] != '*')
+	{
+		Ispisi(polje);
+		Spavaj(1000);
+	}
+	Ispisi(polje);
+	std::cout << "Uspjesno si rijesila zadatak!! Bravo \n";
+}
+
+int main()
+{
+	std::vector<char> red;
+	Ispuni(red);
+	PomakniZvjezdicu(red);
+}
---
+++ Zadatci/Zadatak1/readme.md
@@ -0,0 +1,24 @@
+Ovo je tvoj prvi programmerski zadatak
+
+Pokusao sam da bude sto jednostavnije, ali znas mene..
+ima spicy stvari
+
+Otvori NeRijeseniZadatak.cpp
+Kompajliraj ga.
+Kako?
+1. g++ NeRijeseniZadatak.cpp
+2. ./a.out
+
+Primjeti da program ispisuje
+*____
+i tako u beskonacnost.
+
+Tvoj zadatak je promijenis kod tako da dobijes ovaj ispis
+*____
+_*___
+__*__
+___*_
+____*
+Ako uspijes to napraviti, program ce ti ispisati da si uspjesno obavila zadatak
+
+PS. ako zapnes postoji rjesenje u RijeseniZadatak.cpp
